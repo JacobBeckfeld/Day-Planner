732d02b350bda3987fd916b8cc273205ca2ba066
JacobBeckfeld <jacbeck456@gmail.com>
2021-06-20 18:56:47 -0500

    added times, text areas and buttons and added classes and id's

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+//make today's date
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+//set the text to local storage
+
+//set save buttons to save each individual text input
+
+//set  each text ares color based on the time of day
